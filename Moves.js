29dfd1a90b6e3c0d89bce5e4c00112cb537834a9
Nicholas Petrik <frokrab@gmail.com>
2018-07-21 11:41:15 -0400

    Updated combat buttons for accessibility

--- Moves.js
+++ Moves.js
@@ -5,33 +5,37 @@ class Moves extends React.Component {
   render() {
     return (
       <View style={styles.movesBox}>
-        <Button
-          disabled={this.props.cooldown}
-          onPress={this.props.kick}
-          title='KICK'
-          color='firebrick'
-        />
-        <View style={{height: 10}}></View>
-        <Button
-          disabled={this.props.cooldown}
-          onPress={this.props.spit}
-          title='SPIT'
-          color='firebrick'
-        />
-        <View style={{height: 10}}></View>
-        <Button
-          disabled={this.props.cooldown}
-          onPress={this.props.nap}
-          title='NAP'
-          color='firebrick'
-        />
-        <View style={{height: 10}}></View>
-        <Button
-          disabled={this.props.cooldown}
-          onPress={this.props.charm}
-          title='CHARM'
-          color='firebrick'
-        />
+        <View style={{width: '10%'}}></View>
+        <View style={styles.movesColumn}>
+          <Button
+            disabled={this.props.cooldown}
+            onPress={this.props.nap}
+            title='NAP'
+            color='firebrick'
+          />
+          <Button
+            disabled={this.props.cooldown}
+            onPress={this.props.charm}
+            title='CHARM'
+            color='firebrick'
+          />
+        </View>
+        <View style={{width: '10%'}}></View>
+        <View style={styles.movesColumn}>
+          <Button
+            disabled={this.props.cooldown}
+            onPress={this.props.kick}
+            title='KICK'
+            color='firebrick'
+          />
+          <Button
+            disabled={this.props.cooldown}
+            onPress={this.props.spit}
+            title='SPIT'
+            color='firebrick'
+          />
+        </View>
+        <View style={{width: '10%'}}></View>
       </View>
     );
   }
@@ -39,12 +43,19 @@ class Moves extends React.Component {
 
 const styles = StyleSheet.create({
   movesBox: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     borderColor: 'black',
     borderWidth: 2,
     width: '100%',
     height: '30%',
     backgroundColor: 'darkgray',    
   },
+  movesColumn: {
+    flex: 1,
+    justifyContent: 'space-around',
+  }
 });
 
 export default Moves;
